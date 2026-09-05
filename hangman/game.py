eb7cf254d5edb4e22bc8cd78a780cdf283affbc7
Karthikeyan <tamilkn23@gmail.com>
2026-09-05 21:58:51 +0530

    feat: add lives to the hangman game

--- hangman/game.py
+++ hangman/game.py
@@ -10,12 +10,14 @@ with open('words.json', 'r') as file:
 def join_method(hidden_word):
     return "".join(hidden_word)
 
+lives = 12
 secret_word = choice(words).upper()
 hidden = list("_" * len(secret_word))
 print(f"Hidden word: {join_method(hidden)}")
 
-while "_" in hidden:
+while "_" in hidden and lives > 0:
     guess = input("Enter a letter: ").upper()
+    lives -= 1
 
     if len(guess) != 1 or not guess.isalpha():
         print("Please enter a single letter.")
@@ -29,7 +31,12 @@ while "_" in hidden:
         if char == guess and hidden[i] == '_':
             hidden[i] = char
             print(f"Hidden word: {join_method(hidden)}")
+            print(f"You have {lives} lives left!!!")
             break
     else:
         print("You already found all of those letters.")
-print(f"Finally! you found the fruit: {join_method(hidden)}")
+
+if lives == 0:
+    print("Sorry!!! You lost all your 12 lives!!!")
+else:
+    print(f"Yaahhh!!! You won!!! you found the fruit: {join_method(hidden)}")
